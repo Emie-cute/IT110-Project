03adc80888bb23127ab50b4959435a9c4e178338
Emie-cute <Emie-cute>
2023-12-25 22:26:13 +0800

    First Commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+
+use App\Http\Controllers\AdminController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +17,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+route::get('/',[HomeController::class,'index']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
+
+route::get('/redirect',[HomeController::class,'redirect']);
+
+route::get('/view_category',[AdminController::class,'view_category']);
+
+route::post('/add_category',[AdminController::class,'add_category']);
+
+route::get('/delete_category/{id}',[AdminController::class,'delete_category']);
+
+route::get('/view_product',[AdminController::class,'view_product']);
+
+route::post('/add_product',[AdminController::class,'add_product']);
